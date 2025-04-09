63785f830198d00b781f6c1aff783fcd47e05357
Luis <luisflorentino1999@gmail.com>
2025-04-08 23:34:59 -0400

    added a test class

---
+++ Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Red_Neuronal_Paralela_v1
+{
+    public class Test
+    {
+        public double TestModel(NeuralNetwork nn, double[][] testInputs, double[] testTargets)
+        {
+            double totalError = 0;
+
+            // Itera sobre los datos de prueba
+            for (int i = 0; i < testInputs.Length; i++)
+            {
+                // Obtener la predicción para cada entrada
+                double prediction = nn.Forward(testInputs[i]);
+
+                // Calcular el error cuadrático medio (MSE)
+                double error = Math.Pow(prediction - testTargets[i], 2);
+                totalError += error;
+            }
+
+            // Calcular el error promedio (MSE promedio)
+            return totalError / testInputs.Length;
+        }
+    }
+}
